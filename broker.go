fc5091e02be730b8992b7eee5e2ed1a2a780caf0
rama <yeyrama@gmail.com>
2018-07-04 18:47:56 +0700

    Add google pubsub

--- broker.go
+++ broker.go
@@ -8,7 +8,8 @@ const (
 	E_BROKER_NOT_SUPPORTED = "Broker type not supported"
 )
 const (
-	BROKER_NATS = "nats"
+	BROKER_NATS          = "nats"
+	BROKER_GOOGLE_PUBSUB = "gpubsub"
 )
 
 type Broker interface {
@@ -21,6 +22,8 @@ func NewBroker(selectBroker string, parameters map[string]string) (Broker, error
 	switch selectBroker {
 	case BROKER_NATS:
 		return NewBrokerNats(parameters["url"])
+	case BROKER_GOOGLE_PUBSUB:
+		return NewBrokerGooglePubSub(parameters["google_project"], parameters["google_credentials"])
 	}
 	return nil, errors.New(E_BROKER_NOT_SUPPORTED)
 }
---
+++ broker_gpubsub.go
@@ -0,0 +1,72 @@
+package broke
+
+import (
+	"context"
+	"encoding/json"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"google.golang.org/api/option"
+)
+
+type BrokerGooglePubSub struct {
+	conn *pubsub.Client
+}
+
+const (
+	E_PUBSUB_MESSAGE_NOT_BYTE = "Message must be byte value"
+	PUBSUB_PUBLISH_LIMIT      = 10
+)
+
+func (b *BrokerGooglePubSub) Publish(topic string, message interface{}) (interface{}, error) {
+	data, err := json.Marshal(message)
+	if err != nil {
+		return nil, err
+	}
+	ctx := context.Background()
+	var result *pubsub.PublishResult
+	pubTopic, _ := b.conn.CreateTopic(ctx, topic)
+	result = pubTopic.Publish(ctx, &pubsub.Message{
+		Data: data,
+	})
+	_, err = result.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+func (b *BrokerGooglePubSub) Subscribe(topic string, f func(msg interface{})) (interface{}, error) {
+	ctx := context.Background()
+	pubTopic := b.conn.Topic(topic)
+	sub, err := b.conn.CreateSubscription(ctx, topic, pubsub.SubscriptionConfig{
+		Topic:       pubTopic,
+		AckDeadline: 20 * time.Second,
+	})
+	if err != nil {
+		return nil, err
+	}
+	err = sub.Receive(ctx, func(c context.Context, msg *pubsub.Message) {
+		f(msg.Data)
+	})
+	if err != nil {
+		return nil, err
+	}
+	return nil, nil
+}
+
+func (b *BrokerGooglePubSub) Close() {
+	b.conn.Close()
+}
+
+func NewBrokerGooglePubSub(projectID string, credFile string) (Broker, error) {
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, projectID, option.WithCredentialsFile(credFile))
+	if err != nil {
+		return nil, err
+	}
+	// Create topic if it doesn't exist.
+	return &BrokerGooglePubSub{
+		client,
+	}, nil
+
+}
